a867a3a654e7e40e1aa2e6c61f64ebaa95970a4b
Anuja Khanaj <anujahkhanaj2003@gmail.com>
2024-06-23 20:37:13 +0530

    leetcode code

---
+++ Greatest_Common_Divisor_of_Strings.java
@@ -0,0 +1,14 @@
+public class Greatest_Common_Divisor_of_Strings {
+    
+    public static void main(String[] args) {
+        String str1 = "ABCABC";
+        String str2 = "ABC";
+
+        StringBuilder sb = new StringBuilder();
+
+        int max = Math.max(str1.length(), str2.length());
+        for(int i = 0;i<max;i++){
+            
+        }
+    }
+}
---
+++ Merge_Strings_Alternately.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Merge_Strings_Alternately {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        String word1 = sc.nextLine();
+        String word2 = sc.nextLine();
+        String mergedString = mergeAlternately(word1, word2);
+        System.out.println(mergedString); // Output: "apbqrs"
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int maxLen = Math.max(len1, len2);
+
+        for (int i = 0; i < maxLen; i++) {
+            if (i < len1) {
+                sb.append(word1.charAt(i));
+            }
+           
+            if (i < len2) {
+                sb.append(word2.charAt(i));
+            }
+           
+        }
+
+        return sb.toString();
+    }
+}
