e15b607251f3672cd035f6d0d452739e1e7f65a6
Anuja Khanaj <anujahkhanaj2003@gmail.com>
2025-02-13 07:37:59 +0530

    Sort Colors

---
+++ SortColors.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class SortColors {
+    public static void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                // Swap nums[mid] with nums[low] (0s go to left)
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++; // 1s are in the right place, just move forward
+            } else {
+                // Swap nums[mid] with nums[high] (2s go to right)
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColors(nums);
+        System.out.println(Arrays.toString(nums)); // Output: [0, 0, 1, 1, 2, 2]
+    }
+}
