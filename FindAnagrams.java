1ec9be474881e9822508296dba0041a6d329c27e
Anuja Khanaj <anujahkhanaj2003@gmail.com>
2025-02-15 22:48:15 +0530

    FindAnagrams

--- FindAnagrams.java
+++ FindAnagrams.java
@@ -1,33 +1,30 @@
-import java.util.Arrays;
+import java.util.*;
 
-public class SortColors {
-    public static void sortColors(int[] nums) {
-        int low = 0, mid = 0, high = nums.length - 1;
+public class FindAnagrams {
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        if (s.length() < p.length()) return result;
 
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                // Swap nums[mid] with nums[low] (0s go to left)
-                int temp = nums[mid];
-                nums[mid] = nums[low];
-                nums[low] = temp;
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                mid++; // 1s are in the right place, just move forward
-            } else {
-                // Swap nums[mid] with nums[high] (2s go to right)
-                int temp = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp;
-                high--;
-            }
+        int[] p_count = new int[26];  // Frequency array for p
+        int[] s_count = new int[26];  // Frequency array for s
+
+        for (char c : p.toCharArray()) p_count[c - 'a']++;  // Count p characters
+        for (int i = 0; i < p.length(); i++) s_count[s.charAt(i) - 'a']++;  // First window
+
+        if (Arrays.equals(s_count, p_count)) result.add(0);
+
+        for (int i = p.length(); i < s.length(); i++) {
+            s_count[s.charAt(i) - 'a']++;  // Add new character
+            s_count[s.charAt(i - p.length()) - 'a']--;  // Remove old character
+
+            if (Arrays.equals(s_count, p_count)) 
+                result.add(i - p.length() + 1);
         }
+
+        return result;
     }
 
     public static void main(String[] args) {
-        int[] nums = {2, 0, 2, 1, 1, 0};
-        sortColors(nums);
-        System.out.println(Arrays.toString(nums)); // Output: [0, 0, 1, 1, 2, 2]
+        System.out.println(findAnagrams("cbaebabacd", "abc"));  // Output: [0, 6]
     }
 }
-
